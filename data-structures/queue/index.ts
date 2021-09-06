4e97d973435e78610e0ab8fae91ccaf7cfc9c472
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2021-09-06 23:30:32 +0200

    added queue

---
+++ data-structures/queue/index.ts
@@ -0,0 +1,52 @@
+import { LinkedNode } from '../Node/linked-node';
+
+export class Queue<T> {
+  public head: LinkedNode<T> | null;
+  public tail: LinkedNode<T> | null;
+  public length: number = 0;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  public push(item: T): void {
+    const newList = new LinkedNode<T>(item);
+    if (!this.head) {
+      this.head = newList;
+      this.tail = this.head;
+    } else {
+      newList.prev = this.tail;
+      if (this.tail) {
+        this.tail.next = newList;
+      }
+    }
+    this.tail = newList;
+    this.length++;
+  }
+
+  public pop(): T | null {
+    if (!this.length) return null;
+
+    if (this.head) {
+      const removedValue = this.head.value;
+      this.head = this.head.next;
+      this.length--;
+      return removedValue;
+    }
+    return null;
+  }
+
+  public selectByIndex(index: number): T | null {
+    let current = this.head;
+    let count: number = 0;
+    while (current !== null) {
+      if (count === index && current) {
+        return current.value;
+      }
+      count++;
+      current = current.next;
+    }
+    return null;
+  }
+}
